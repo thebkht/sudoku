5dd7537e7ac6f82e8f01b0eef40015a2b0ff7698
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-04-16 16:18:13 +0900

    init shadcn-ui

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "bkhtdev/sudoku",
+  url: "https://sudoku.bkhtdev.com",
+  ogImage: "https://sudoku.bkhtdev.com/og.png",
+  description:
+    "Sudoku game with a focus on accessibility and performance. Playable with keyboard, mouse, or touch. Supports dark mode, PWA, and offline play.",
+  links: {
+    twitter: "https://twitter.com/yusupovbg",
+    github: "https://github.com/yusupovbg/sudoku",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
